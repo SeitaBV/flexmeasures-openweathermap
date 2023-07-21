6db4cc0ff2956f93eb9ba7548e53293aa026677c
Ahmad Wahid <59763365+Ahmad-Wahid@users.noreply.github.com>
2023-07-21 13:45:02 +0500

    Update OWM API version to 3 and auto version checker (#12)
    * change owm version to 3 and add version checker
    
    * rais a warning
    
    * try black only
    
    * make API_VERSION a module constant and add tested versions to the warning
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    
    ---------
    
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>
    Co-authored-by: Nicolas Höning <nicolas@seita.nl>

--- flexmeasures_openweathermap/utils/owm.py
+++ flexmeasures_openweathermap/utils/owm.py
@@ -20,6 +20,9 @@ from .modeling import (
 )
 from .radiating import compute_irradiance
 
+    
+API_VERSION = "3.0"
+
 
 def get_supported_sensor_spec(name: str) -> Optional[dict]:
     """
@@ -49,8 +52,11 @@ def call_openweatherapi(
     See https://openweathermap.org/api/one-call-api for docs.
     Note that the first forecast is about the current hour.
     """
+    check_openweathermap_version(API_VERSION)
     query_str = f"lat={location[0]}&lon={location[1]}&units=metric&exclude=minutely,daily,alerts&appid={api_key}"
-    res = requests.get(f"http://api.openweathermap.org/data/2.5/onecall?{query_str}")
+    res = requests.get(
+        f"http://api.openweathermap.org/data/{API_VERSION}/onecall?{query_str}"
+    )
     assert (
         res.status_code == 200
     ), f"OpenWeatherMap returned status code {res.status_code}: {res.text}"
@@ -211,3 +217,11 @@ def save_forecasts_as_json(
         )
         with open(forecasts_file, "w") as outfile:
             json.dump(forecasts, outfile)
+
+
+def check_openweathermap_version(api_version: str):
+    supported_versions = ["2.5", "3.0"]
+    if api_version not in supported_versions:
+        current_app.logger.warning(
+            f"This plugin may not be fully compatible with OpenWeatherMap API version {api_version}. We tested with versions {supported_versions}"
+        )

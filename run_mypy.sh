0039e01635215d3e47bdfaba4a1c27943d365068
Nicolas Höning <nicolas@seita.nl>
2024-10-09 08:54:51 +0200

    make find_weather_sensor_by_location more strict about failing to find a sensor close enough (#30)
    Signed-off-by: Nicolas Höning <nicolas@seita.nl>

--- run_mypy.sh
+++ run_mypy.sh
@@ -2,5 +2,5 @@
 set -e
 pip install --upgrade mypy > 1.4
 pip install types-pytz types-requests types-Flask types-click types-redis types-tzlocal types-python-dateutil types-setuptools
-files=$(find . -name \*.py -not -path "./venv/*" -not -path ".eggs/*" -not -path "./build/*" -not -path "./dist/*" -not -path "./scripts/*")
+files=$(find flexmeasures_openweathermap -name \*.py)
 mypy --follow-imports skip --ignore-missing-imports $files 
